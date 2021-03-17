1c303c36267505ec78730437a98d3ff42e6d6742
Keira Dueck <sylae@calref.net>
2021-03-17 04:27:36 -0700

    Delete All Reactions for Emoji

--- src/HTTP/Endpoints/Channel.php
+++ src/HTTP/Endpoints/Channel.php
@@ -26,15 +26,16 @@ class Channel {
             'list' => 'channels/%s/messages',
             'get' => 'channels/%s/messages/%s',
             'create' => 'channels/%s/messages',
-            'reactions' => array(
+            'reactions' => [
                 'create' => 'channels/%s/messages/%s/reactions/%s/@me',
                 'delete' => 'channels/%s/messages/%s/reactions/%s/%s',
+                'deleteEmoji' => 'channels/%s/messages/%s/reactions/%s',
                 'get' => 'channels/%s/messages/%s/reactions/%s',
                 'deleteAll' => 'channels/%s/messages/%s/reactions',
-            ),
+            ],
             'edit' => 'channels/%s/messages/%s',
             'delete' => 'channels/%s/messages/%s',
-            'bulkDelete' => 'channels/%s/messages/bulk-delete'
+            'bulkDelete' => 'channels/%s/messages/bulk-delete',
         ),
         'permissions' => array(
             'edit' => 'channels/%s/permissions/%s',
@@ -55,12 +56,12 @@ class Channel {
             'remove' => 'channels/%s/recipients/%s'
         )
     );
-    
+
     /**
      * @var \CharlotteDunois\Yasmin\HTTP\APIManager
      */
     protected $api;
-    
+
     /**
      * Constructor.
      * @param \CharlotteDunois\Yasmin\HTTP\APIManager $api
@@ -68,117 +69,132 @@ class Channel {
     function __construct(\CharlotteDunois\Yasmin\HTTP\APIManager $api) {
         $this->api = $api;
     }
-    
+
     function getChannel(string $channelid) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['get'], $channelid);
         return $this->api->makeRequest('GET', $url, array());
     }
-    
+
     function modifyChannel(string $channelid, array $data, string $reason = '') {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['modify'], $channelid);
         return $this->api->makeRequest('PATCH', $url, array('auditLogReason' => $reason, 'data' => $data));
     }
-    
+
     function deleteChannel(string $channelid, string $reason = '') {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['delete'], $channelid);
         return $this->api->makeRequest('DELETE', $url, array('auditLogReason' => $reason));
     }
-    
+
     function getChannelMessages(string $channelid, array $options = array()) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['list'], $channelid);
         return $this->api->makeRequest('GET', $url, array('querystring' => $options));
     }
-    
+
     function getChannelMessage(string $channelid, string $messageid) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['get'], $channelid, $messageid);
         return $this->api->makeRequest('GET', $url, array());
     }
-    
+
     function createMessage(string $channelid, array $options, array $files = array()) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['create'], $channelid);
         return $this->api->makeRequest('POST', $url, array('data' => $options, 'files' => $files));
     }
-    
+
     function editMessage(string $channelid, string $messageid, array $options) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['edit'], $channelid, $messageid);
         return $this->api->makeRequest('PATCH', $url, array('data' => $options));
     }
-    
+
     function deleteMessage(string $channelid, string $messageid, string $reason = '') {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['delete'], $channelid, $messageid);
         return $this->api->makeRequest('DELETE', $url, array('auditLogReason' => $reason));
     }
-    
+
     function bulkDeleteMessages(string $channelid, array $snowflakes, string $reason = '') {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['bulkDelete'], $channelid);
         return $this->api->makeRequest('POST', $url, array('auditLogReason' => $reason, 'data' => array('messages' => $snowflakes)));
     }
-    
+
     function createMessageReaction(string $channelid, string $messageid, string $emoji) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['reactions']['create'], $channelid, $messageid, $emoji);
         return $this->api->makeRequest('PUT', $url, array('reactionRatelimit' => true));
     }
-    
+
     function deleteMessageUserReaction(string $channelid, string $messageid, string $emoji, string $userid) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['reactions']['delete'], $channelid, $messageid, $emoji, $userid);
         return $this->api->makeRequest('DELETE', $url, array('reactionRatelimit' => true));
     }
-    
-    function getMessageReactions(string $channelid, string $messageid, string $emoji, array $querystring = array()) {
-        $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['reactions']['get'], $channelid, $messageid, $emoji);
-        return $this->api->makeRequest('GET', $url, array('querystring' => $querystring));
+
+    function getMessageReactions(string $channelid, string $messageid, string $emoji, array $querystring = [])
+    {
+        $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['reactions']['get'],
+            $channelid, $messageid, $emoji);
+        return $this->api->makeRequest('GET', $url, ['querystring' => $querystring]);
     }
-    
-    function deleteMessageReactions(string $channelid, string $messageid) {
-        $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['reactions']['deleteAll'], $channelid, $messageid);
-        return $this->api->makeRequest('DELETE', $url, array());
+
+    function deleteMessageReactions(string $channelid, string $messageid)
+    {
+        $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['reactions']['deleteAll'],
+            $channelid, $messageid);
+        return $this->api->makeRequest('DELETE', $url, []);
     }
-    
-    function editChannelPermissions(string $channelid, string $overwriteid, array $options, string $reason = '') {
-        $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['permissions']['edit'], $channelid, $overwriteid);
-        return $this->api->makeRequest('PUT', $url, array('auditLogReason' => $reason, 'data' => $options));
+
+    function deleteMessageReactionsForEmoji(string $channelid, string $messageid, string $emoji)
+    {
+        $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['messages']['reactions']['deleteEmoji'],
+            $channelid, $messageid, $emoji);
+        return $this->api->makeRequest('DELETE', $url, []);
     }
-    
-    function deleteChannelPermission(string $channelid, string $overwriteid, string $reason = '') {
-        $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['permissions']['delete'], $channelid, $overwriteid);
-        return $this->api->makeRequest('DELETE', $url, array('auditLogReason' => $reason));
+
+    function editChannelPermissions(string $channelid, string $overwriteid, array $options, string $reason = '')
+    {
+        $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['permissions']['edit'], $channelid,
+            $overwriteid);
+        return $this->api->makeRequest('PUT', $url, ['auditLogReason' => $reason, 'data' => $options]);
     }
-    
+
+    function deleteChannelPermission(string $channelid, string $overwriteid, string $reason = '')
+    {
+        $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['permissions']['delete'], $channelid,
+            $overwriteid);
+        return $this->api->makeRequest('DELETE', $url, ['auditLogReason' => $reason]);
+    }
+
     function getChannelInvites(string $channelid) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['invites']['list'], $channelid);
         return $this->api->makeRequest('GET', $url, array());
     }
-    
+
     function createChannelInvite(string $channelid, array $options = array()) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['invites']['create'], $channelid);
         return $this->api->makeRequest('POST', $url, array('data' => $options));
     }
-    
+
     function triggerChannelTyping(string $channelid) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['typing'], $channelid);
         return $this->api->makeRequest('POST', $url, array());
     }
-    
+
     function getPinnedChannelMessages(string $channelid) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['pins']['list'], $channelid);
         return $this->api->makeRequest('GET', $url, array());
     }
-    
+
     function pinChannelMessage(string $channelid, string $messageid) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['pins']['add'], $channelid, $messageid);
         return $this->api->makeRequest('PUT', $url, array());
     }
-    
+
     function unpinChannelMessage(string $channelid, string $messageid) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['pins']['delete'], $channelid, $messageid);
         return $this->api->makeRequest('DELETE', $url, array());
     }
-    
+
     function groupDMAddRecipient(string $channelid, string $userid, string $accessToken, string $nick) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['groupDM']['add'], $channelid, $userid);
         return $this->api->makeRequest('PUT', $url, array('data' => array('access_token' => $accessToken, 'nick' => $nick)));
     }
-    
+
     function groupDMRemoveRecipient(string $channelid, string $userid) {
         $url = \CharlotteDunois\Yasmin\HTTP\APIEndpoints::format(self::ENDPOINTS['groupDM']['remove'], $channelid, $userid);
         return $this->api->makeRequest('DELETE', $url, array());
--- src/Models/MessageReaction.php
+++ src/Models/MessageReaction.php
@@ -24,31 +24,31 @@ class MessageReaction extends ClientBase {
      * @var \CharlotteDunois\Yasmin\Models\Message
      */
     protected $message;
-    
+
     /**
      * The emoji this message reaction is for.
      * @var \CharlotteDunois\Yasmin\Models\Emoji
      */
     protected $emoji;
-    
+
     /**
      * Times this emoji has been reacted.
      * @var int
      */
     protected $count;
-    
+
     /**
      * Whether the current user has reacted using this emoji.
      * @var bool
      */
     protected $me;
-    
+
     /**
      * The users that have given this reaction, mapped by their ID.
      * @var \CharlotteDunois\Collect\Collection
      */
     protected $users;
-    
+
     /**
      * @internal
      */
@@ -56,12 +56,12 @@ class MessageReaction extends ClientBase {
         parent::__construct($client);
         $this->message = $message;
         $this->emoji = $emoji;
-        
+
         $this->count = (int) $reaction['count'];
         $this->me = (bool) $reaction['me'];
         $this->users = new \CharlotteDunois\Collect\Collection();
     }
-    
+
     /**
      * {@inheritdoc}
      * @return mixed
@@ -72,10 +72,10 @@ class MessageReaction extends ClientBase {
         if(\property_exists($this, $name)) {
             return $this->$name;
         }
-        
+
         return parent::__get($name);
     }
-    
+
     /**
      * Fetches all the users that gave this reaction. Resolves with a Collection of User instances, mapped by their IDs.
      * @param int     $limit   The maximum amount of users to fetch, defaults to 100.
@@ -87,15 +87,15 @@ class MessageReaction extends ClientBase {
     function fetchUsers(int $limit = 100, string $before = '', string $after = '') {
         return (new \React\Promise\Promise(function (callable $resolve, callable $reject) use ($limit, $before, $after) {
             $query = array('limit' => $limit);
-            
+
             if(!empty($before)) {
                 $query['before'] = $before;
             }
-            
+
             if(!empty($after)) {
                 $query['after'] = $after;
             }
-            
+
             $this->client->apimanager()->endpoints->channel
                 ->getMessageReactions($this->message->channel->getId(), $this->message->id, $this->emoji->identifier, $query)
                 ->done(function ($data) use ($resolve) {
@@ -103,32 +103,41 @@ class MessageReaction extends ClientBase {
                         $user = $this->client->users->patch($react);
                         $this->users->set($user->id, $user);
                     }
-                    
+
                     $resolve($this->users);
                 }, $reject);
         }));
     }
-    
+
     /**
-     * Removes an user from the reaction. Resolves with $this.
-     * @param \CharlotteDunois\Yasmin\Models\User|string  $user  Defaults to the client user.
+     * Remove all reactions, or specify a User to remove only theirs. Resolves with $this.
+     *
+     * @param \CharlotteDunois\Yasmin\Models\User|string $user Defaults to all users.
+     *
      * @return \React\Promise\ExtendedPromiseInterface
      * @throws \InvalidArgumentException
      */
     function remove($user = null) {
-        if($user !== null) {
-            $user = $this->client->users->resolve($user);
-        }
-        
         return (new \React\Promise\Promise(function (callable $resolve, callable $reject) use ($user) {
-            $this->client->apimanager()->endpoints->channel
-                ->deleteMessageUserReaction($this->message->channel->getId(), $this->message->id, $this->emoji->identifier, ($user !== null ? $user->id : '@me'))
-                ->done(function () use ($resolve) {
-                    $resolve($this);
-                }, $reject);
+            if ($user !== null) {
+                $this->client->apimanager()->endpoints->channel
+                    ->deleteMessageUserReaction($this->message->channel->getId(), $this->message->id,
+                        $this->emoji->identifier, $user->id)
+                    ->done(function () use ($resolve) {
+                        $resolve($this);
+                    }, $reject);
+            } else {
+                $this->client->apimanager()->endpoints->channel
+                    ->deleteMessageReactionsForEmoji($this->message->channel->getId(), $this->message->id,
+                        $this->emoji->identifier)
+                    ->done(function () use ($resolve) {
+                        $resolve($this);
+                    }, $reject);
+            }
+
         }));
     }
-    
+
     /**
      * Increments the count.
      * @return void
@@ -137,7 +146,7 @@ class MessageReaction extends ClientBase {
     function _incrementCount() {
         $this->count++;
     }
-    
+
     /**
      * Decrements the count.
      * @return void
